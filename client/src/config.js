8e702976feb40366441d2c8a6205bb7d5dddfdac
Naveeth6 <rbnaveeth@gmail.com>
2026-01-12 21:25:11 +0530

    Update API URL to production backend

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 const config = {
-    API_URL: import.meta.env.PROD ? '' : 'http://localhost:5000'
+    API_URL: import.meta.env.PROD ? 'https://studioproject-backend.onrender.com' : 'http://localhost:5000'
 };
 
 export default config;
