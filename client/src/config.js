bb7fa388bad18c0d91d08f3f69a4b9fedadf1c88
Naveeth6 <rbnaveeth@gmail.com>
2026-01-23 11:14:16 +0400

    update on 23 11:00

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 const config = {
-    API_URL: import.meta.env.PROD ? 'https://studioproject-backend.onrender.com' : 'http://localhost:5000'
+    API_URL: import.meta.env.PROD ? 'https://freezstudio-backend.onrender.com' : 'http://localhost:5000'
 };
 
 export default config;
